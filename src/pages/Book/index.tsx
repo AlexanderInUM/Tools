1765636feac7aa24f3d5d57b1e08f7166439da2d
BlackBerry009 <765042894@qq.com>
2023-02-15 21:54:21 +0800

    css2tailwindcss

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,17 +1,8 @@
-import { CSSProperties } from 'react';
-
-const iframeStyle: CSSProperties = {
-  width: '100%',
-  height: '80vh',
-  border: 'none',
-  display: 'block'
-};
-
 export default function Book() {
   return (
     <iframe
       src='https://zlibsearch.1kbtool.com/'
-      style={iframeStyle}
+      className='block h-[80vh] w-full border-none'
       title='zlib'
     ></iframe>
   );
